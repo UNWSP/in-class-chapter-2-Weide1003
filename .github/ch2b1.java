45324f32aef5e305dce33ae24c24f13f8aff0e6a
Weide1003 <112137004+Weide1003@users.noreply.github.com>
2022-09-01 20:42:55 -0500

    Add files via upload

---
+++ .github/ch2b1.java
@@ -0,0 +1,15 @@
+package ch2a2;
+
+public class ch2b1 {
+final int EXAMPLE = 4;
+
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int EXAMPLE = 5;
+		System.out.println(EXAMPLE);
+		
+
+	}
+
+}
